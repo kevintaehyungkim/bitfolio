4e66399cf395b564e6542ef9934aeb17b9710a09
Kevin Kim <kevintaehyungkim@Kevins-MacBook-Pro.local>
2017-12-25 11:59:25 +0900

    added signup page

--- crypto.py
+++ crypto.py
@@ -2,19 +2,28 @@
 # http://localhost:5000/
 import requests
 import json
-
 from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
+# home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# dashboard
 @app.route('/user/<user>')
 def hello(user):
-    return render_template('dashboard.html', name=user)
+    return render_template('dashboard.html', name=user, total_val=receive_data())
 
+
+# signup page
+@app.route('/signup')
+def home():
+    return render_template('signup.html')
+
+
+# login page
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
@@ -29,8 +38,11 @@ def login():
 def receive_data():
     url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD"
     response = requests.get(url)
-    data = response.content
-    print (data)
+    data = response.json()
+    return (data["BTC"]["USD"])
+    # print (data[0])
+    # print (data[1])
+    # print (data)
 
 # with coin/quantities, calculate graph based on 
 def calculate_total(coin_dict):
@@ -40,5 +52,6 @@ def update_total():
     return
 
 if __name__ == "__main__":
+    receive_data()
     app.run(debug=True)
     print ("app running")
